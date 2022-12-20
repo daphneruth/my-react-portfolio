a0911fb6a135f38e4c486f38dc7f811f4e14ef12
daphneruth <daphneruth304@gmail.com>
2022-12-20 12:23:42 +0300

    updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import doc from "../../Assets/../Assets/daphResume.docx";
+import doc from "../../Assets/../Assets/daph.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
