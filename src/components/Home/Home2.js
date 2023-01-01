288978ef9498af82fd427e36d8bfa78ec7b65f99
daphneruth <daphneruth304@gmail.com>
2023-01-01 11:26:17 +0300

    updated

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -21,9 +21,9 @@ function Home2() {
             <p className="home-about-body">
               I am highly competent full stack developer.
               <br />
-              <br />I am fluent in
+              <br />Ask me about
               <i>
-                <b className="purple"> Javascript ,ReactJs and NodeJs. </b>
+                <b className="purple"> React.Js and Node.Js </b>
               </i>
               <br />
               <br />
